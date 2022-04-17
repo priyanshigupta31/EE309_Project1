0101ec7ab4a5437996d46b802ac695e710d749bf
sakshiheda <92177769+sakshiheda@users.noreply.github.com>
2022-04-17 17:46:14 +0530

    Update controloutput.vhdl

--- controloutput.vhdl
+++ controloutput.vhdl
@@ -2,7 +2,9 @@
         port(ir: in std_logic_vector (15 downto 0);
             controlword_ALU: out std_logic_vector (3 downto 0);
             controlword_Mem: out std_logic_vector (2 downto 0);
-            controlword_Reg: out std_logic_vector (3 downto 0);
+            controlword_Reg1: out std_logic_vector (2 downto 0);
+            controlword_Reg2: out std_logic_vector (2 downto 0);
+            controlword_Reg3: out std_logic_vector (2 downto 0);
             controlword_Ls: out std_logic_vector (1 downto 0);
             controlword_SE1: out std_logic_vector (1 downto 0);
             controlword_SE2: out std_logic_vector (1 downto 0);
@@ -25,6 +27,9 @@
         if y_present = S0 then                      --1
             controlword_ALU = 0001;
             controlword_Mem = 001;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -42,6 +47,9 @@
         elsif y_present = S_ar then                 --2
             controlword_ALU = 0010;
             controlword_Mem = 000;
+            controlword_Reg1 = 001;
+            controlword_Reg2 = 001;
+            controlword_Reg3 = 001;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -59,6 +67,9 @@
         elsif y_present = S_ar_ls then              --3
             controlword_ALU = 0011;
             controlword_Mem = 000;
+            controlword_Reg1 = 001;
+            controlword_Reg2 = 010;
+            controlword_Reg3 = 001;
             controlword_Ls = 001;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -76,6 +87,9 @@
         elsif y_present = S_adi then                --4
             controlword_ALU = 0100;
             controlword_Mem = 000;
+            controlword_Reg1 = 001;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 010;
             controlword_Ls = 000;
             controlword_SE1 = 001;
             controlword_SE2 = 000;
@@ -93,6 +107,9 @@
         elsif y_present = s_lhi then                --5
             controlword_ALU = 000;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 011;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 001;
@@ -110,6 +127,9 @@
         elsif y_present = S_ls then                 --6
             controlword_ALU = 0101;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 001;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 010;
             controlword_SE2 = 000;
@@ -127,6 +147,9 @@
         elsif y_present = s_l then                  --7
             controlword_ALU = 0000;
             controlword_Mem = 010;
+            controlword_Reg1 = 010;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -144,6 +167,9 @@
         elsif y_present = s_s then                  --8
             controlword_ALU = 0000;
             controlword_Mem = 011;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 011;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -161,6 +187,9 @@
         elsif y_present = s_lm0 then                --9
             controlword_ALU = 000;
             controlword_Mem = 000;
+            controlword_Reg1 = 011;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -178,6 +207,9 @@
         elsif y_present = s_lm1 then                --10
             controlword_ALU = 000;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 010;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -195,6 +227,9 @@
         elsif y_present = S_lm2 then                --11
             controlword_ALU = 1011;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -212,6 +247,9 @@
         elsif y_present = s_lm3 then                --12
             controlword_ALU = 0000;
             controlword_Mem = 100;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 100;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -228,7 +266,10 @@
 
         elsif y_present = S_lm4 then                --13
             controlword_ALU = 1100;
-            controlword_Mem = 000;    
+            controlword_Mem = 000;   
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -246,6 +287,9 @@
         elsif y_present = s_sm3 then                --14
             controlword_ALU = 0000;
             controlword_Mem = 011;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 101;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -263,6 +307,9 @@
         elsif y_present = S_beq0 then               --15          
             controlword_ALU = 0110;
             controlword_Mem = 000;
+            controlword_Reg1 = 001;
+            controlword_Reg2 = 001;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -280,6 +327,9 @@
         elsif y_present = S_beq1 then               --16
             controlword_ALU = 1001;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 001;
             controlword_SE2 = 000;
@@ -297,6 +347,9 @@
         elsif y_present = S_0b then                 --17
             controlword_ALU = 0111;
             controlword_Mem = 001;
+            controlword_Reg1 = 100;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -314,6 +367,9 @@
         elsif y_present = S_jlr then               --18
             controlword_ALU = 000;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 110;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 010;
@@ -331,6 +387,9 @@
         elsif y_present = S_jri1 then               --19
             controlword_ALU = 1000;
             controlword_Mem = 000;
+            controlword_Reg1 = 001;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 000;
             controlword_Ls = 000;
             controlword_SE1 = 000;
             controlword_SE2 = 000;
@@ -348,6 +407,9 @@
         elsif y_present = S_jri2 then               --20
             controlword_ALU = 1010;
             controlword_Mem = 000;
+            controlword_Reg1 = 000;
+            controlword_Reg2 = 000;
+            controlword_Reg3 = 100;
             controlword_Ls = 000; 
             controlword_SE1 = 000;
             controlword_SE2 = 000;
